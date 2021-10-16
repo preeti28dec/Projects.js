2e35406014e93a79497b586449914a3162468f18
preeti28dec <79787988+preeti28dec@users.noreply.github.com>
2021-10-16 10:41:08 +0530

    Quotes world

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// write a function named changeColor  
-    // - get the container1 color value by using getElementById and store it in a variable
-    // - get the input1 value by using getElementById and store it in a variable
-    // - update the background color of container1 by the input1 value
-    // - And clear the input that user enter to empty value
-function changeColour(){
-    var color = document.getElementById("container1")
-    var input = document.getElementById("input1")
-    color.style.backgroundColor = input.value;
-    input.value = null;
+var text=document.getElementById("quote");
+var author=document.getElementById("author");
+var getNewQuote = async () =>
+{
+    //api for quotes
+    var url="https://type.fit/api/quotes";    
+    // fetch the data from api
+    var response=await fetch(url);
+    console.log(typeof response);
+    //convert response to json and store it in quotes array
+    var allQuotes = await response.json();
+    // Generates a random number between 0 and the length of the quotes array
+    var indx = Math.floor(Math.random()*allQuotes.length);
+    //Store the quote present at the randomly generated index
+    var quote=allQuotes[indx].text;  
+    //Store the author of the respective quote
+    var auth=allQuotes[indx].author;
+    if(auth==null)
+    {
+        author = "Anonymous";
+    } 
+    //function to dynamically display the quote and the author
+    text.innerHTML=quote;
+    author.innerHTML="~ "+auth;
 }
+getNewQuote(); 
